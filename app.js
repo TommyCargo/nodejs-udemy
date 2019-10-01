c65cd6a3e6a7d6b60f6371ea533ec641581051c1
Nickolai Chernyi <s0fk@ya.ru>
2019-10-01 11:31:35 +0300

    testing connection to database

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const { getPosts } = require('./routes/post');
+mongoose.connect(
+    process.env.MONGO_URI,
+    {useNewUrlParser: true, useUnifiedTopology: true}
+  )
+  .then(() => console.log('DB Connected'))
+  .catch(err => console.log(err));
 
-const myOwnMiddleware = (req, res, next) => {
-    console.log('middleware applied!');
-    next();
-};
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`);
+});
+
+const postRoutes = require('./routes/post');
 
 app.use(morgan("dev"));
-app.use(myOwnMiddleware);
 
-app.get('/', getPosts);
+app.use('/', postRoutes);
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`A Node JS API is listening on port: ${port}`);
 });
